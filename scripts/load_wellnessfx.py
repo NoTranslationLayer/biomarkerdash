9d5598340069e0a0bfd77a526fe17979b7984f92
Syler <syler@syler.xyz>
2023-08-16 10:28:37 -0400

    Separate pages for separate categories

--- scripts/load_wellnessfx.py
+++ scripts/load_wellnessfx.py
@@ -5,79 +5,18 @@ import pandas as pd
 import sys
 import biomarkerdash.utils as util
 import biomarkerdash.plotting as plot
+import biomarkerdash.html as htm
 import biomarkerdash.biomarker as bm
 from typing import List, Dict, Optional, Tuple
 
 
 import yaml
 
+
 def load_categories(filename: str) -> Dict:
     """Load biomarker categories from a YAML file."""
-    with open(filename, 'r', encoding='utf-8') as f:
+    with open(filename, "r", encoding="utf-8") as f:
         return yaml.safe_load(f)
-
-def combine_html_files(plot_html_list: List[str], output_file: str):
-    """
-    Combine multiple HTML files into a single page.
-
-    Args:
-    - plot_html_list (List[str]): List of HTML contents to combine.
-    - output_file (str): Name of the combined output file.
-    """
-
-    # Header
-    combined_html = """
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <style>
-            body {
-                font-family: "proxima-nova-soft", "proxima-nova", Helvetica, Arial, sans-serif;
-                font-weight: 500;
-                line-height: 1.2;
-                color: #555;
-            }
-            
-            h2 {
-                border-bottom: 1px solid lightgrey;
-                padding-bottom: 10px;
-            }
-
-            /* Added these styles for links */
-            a {
-                text-decoration: none;
-                color: inherit;
-            }
-
-            a:hover {
-                text-decoration: underline;
-            }
-        </style>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Combined Plots</title>
-    </head>
-    <body>
-    """
-
-    # Add Table of Contents
-    combined_html += "<h1>Table of Contents</h1>"
-    combined_html += "<ul>"
-    for category, subcategories in categories.items():    
-        combined_html += f'<li><a href="#{category}">{category}</a></li>'
-    combined_html += "</ul>"
-    combined_html += "<hr>"
-
-    for plot_html in plot_html_list:
-        combined_html += plot_html
-
-    # Footer
-    combined_html += "</body></html>"
-
-    # Write combined content to file
-    with open(output_file, 'w', encoding='utf-8') as f:
-        f.write(combined_html)
 
 
 if __name__ == "__main__":
@@ -91,14 +30,14 @@ if __name__ == "__main__":
     plot_output_dir = "_includes"
     os.makedirs(plot_output_dir, exist_ok=True)
 
+    categories = load_categories("categories.yaml")
 
-    categories = load_categories('categories.yaml')
-
-    html_content = []
+    output_files = []
 
     for category, subcategories in categories.items():
+        html_content = []
         html_content.append(f'<h2 id="{category}">{category}</h2>')
-        
+
         for subcategory, biomarkers_list in subcategories.items():
             html_content.append(f'<h3 id="{subcategory}">{subcategory}</h3>')
 
@@ -106,8 +45,10 @@ if __name__ == "__main__":
             for marker_name in biomarkers_list:
                 marker_obj = biomarkers.get(marker_name)
                 if marker_obj:  # Check if biomarker data exists
-                    filename = os.path.join(plot_output_dir, util.generate_filename(marker_name))
-                    
+                    filename = os.path.join(
+                        plot_output_dir, util.generate_filename(marker_name)
+                    )
+
                     # Save the individual plot
                     plot.plot_history(marker_obj, save_to=filename)
                     print(f"Plot for {marker_name} saved to {filename}.")
@@ -116,8 +57,29 @@ if __name__ == "__main__":
                     if not os.path.exists(filename):
                         print(f"{filename} not found, skipping")
                         continue
-                    with open(filename, 'r', encoding='utf-8') as f:
+                    with open(filename, "r", encoding="utf-8") as f:
                         html_content.append(f.read())
 
-    combine_html_files(html_content, "dashboard.html")
+        output_file = htm.combine_html_files(category, html_content, ".")
+        output_files.append(output_file)
+
+    # Now that all category files have been generated, create the TOC
+    header = htm.create_header_toc(
+        {cat: util.generate_filename(cat) for cat in categories.keys()}
+    )
+
+    # Prepend TOC to each category file
+    for output_file in output_files:
+        with open(output_file, "r+", encoding="utf-8") as f:
+            content = f.read()
+            f.seek(0, 0)
+            f.write(header + content)
+
+    index_page_filename = "dashboard.html"
+    index_page_html_content = header
+    index_page_html_content += "</body></html>"
+
+    with open(index_page_filename, "w", encoding="utf-8") as f:
+        f.write(index_page_html_content)
+
     print("All plots combined into dashboard.html.")

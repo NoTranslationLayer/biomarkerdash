928c282b659500b271cb507245de09383c18895c
Syler <syler@syler.xyz>
2023-08-15 20:36:33 -0400

    Revert "Just set Y range based on data min and max, not ref ranges"
    This reverts commit c272a63de2c57809109d4a7ed5b0d028c59bdd07.

--- biomarkerdash/plotting.py
+++ biomarkerdash/plotting.py
@@ -76,12 +76,12 @@ def plot_history(marker: bm.Biomarker, save_to: str) -> None:
     data_max = max(values)
 
     # Define a buffer for y-axis (e.g., 10% of max_val)
-    buffer = 0.1 * data_max
+    buffer = 0.1 * (max_val if max_val is not None else data_max)
     
     # Determine the overall y-axis range
     y_range = [
-        data_min - buffer,
-        data_max + buffer
+        min(min_val - buffer if min_val is not None else data_min, data_min - buffer),
+        max(max_val + buffer if max_val is not None else data_max, data_max + buffer)
     ]
     shapes = []
 

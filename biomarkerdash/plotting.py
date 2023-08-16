b0f0478dd7169693b0fd93be687626ba79ad0318
Syler <syler@syler.xyz>
2023-08-16 16:37:29 -0400

    Add license to source files

--- biomarkerdash/plotting.py
+++ biomarkerdash/plotting.py
@@ -1,3 +1,35 @@
+# filename: plotting.py
+# Utilities to plot biomarker time series data
+
+# Copyright (c) 2023, No Translation Layer LLC
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 import numpy as np
 import plotly.graph_objects as go
 import plotly.offline as pyo
@@ -10,6 +42,7 @@ from biomarkerdash.constants import (
     COLOR_LINE,
     COLOR_BG_OUTSIDE_REF_RANGE,
 )
+
 
 def determine_color(
     value: float, ref_range: Tuple[Optional[float], Optional[float]]
@@ -139,7 +172,7 @@ def plot_history(marker: bm.Biomarker, save_to: str) -> None:
             text=f"{marker.name} <br><sup>{marker.description}</sup>",
             xref="paper",
             x=0,
-            font={**font_family, "size": 18}
+            font={**font_family, "size": 18},
         ),
         title_x=0.5,
         title_y=0.93,
@@ -148,18 +181,18 @@ def plot_history(marker: bm.Biomarker, save_to: str) -> None:
         xaxis_title="Date",
         xaxis=dict(
             title_font={**font_family, "size": 14},
-            tickfont={**font_family, "size": 12}
+            tickfont={**font_family, "size": 12},
         ),
         yaxis_title=f"Value ({marker.unit})",
         yaxis=dict(
             title_font={**font_family, "size": 14},
-            tickfont={**font_family, "size": 12}
+            tickfont={**font_family, "size": 12},
         ),
         shapes=shapes,
         yaxis_range=y_range,
         showlegend=False,
         plot_bgcolor="rgba(0,0,0,0.04)",
-        font={**font_family, "size": 12}
+        font={**font_family, "size": 12},
     )
 
     pyo.plot(fig, filename=save_to, auto_open=False)
